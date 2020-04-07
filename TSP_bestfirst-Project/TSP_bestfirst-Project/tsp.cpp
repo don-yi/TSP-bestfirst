f80978bcb14e55c7aceab56af3dd60cfaf658dd1
Dennis <dennis.donguk.lee@gmail.com>
2020-04-07 11:25:02 +0900

    File read-in and resize map

--- TSP_bestfirst-Project/TSP_bestfirst-Project/tsp.cpp
+++ TSP_bestfirst-Project/TSP_bestfirst-Project/tsp.cpp
@@ -1,7 +1,39 @@
+#include <fstream>
+#include <sstream>
+
 #include "tsp.h"
 
 std::vector<int> SolveTSP(char const* filename)
 {
+  // 
+  std::vector<int> res(1, 0);
+  std::stringstream ss{};
 
-  return {};
+  // open up file
+  std::ifstream ifs(filename);
+  if (!ifs.is_open())
+  {
+    //abort();
+    return res;
+  }
+
+  // read in
+  ss << ifs.rdbuf();
+
+  int citySize = 0;
+  ss >> citySize;
+
+  MAP map(citySize);
+
+  for (int i = 0; i < citySize; ++i)
+  {
+    map[i].resize(
+      citySize - 1 - i, std::numeric_limits<int>::max()
+    );
+  }
+
+  // close file
+  ifs.close();
+
+  return res;
 }
